b8406728708f615f66ce542bcf616e75b1cfd7e9
John Carry <carryjohn071@gmail.com>
2020-04-11 20:05:32 +0100

    Today's class update

---
+++ src/test/java/com/demo/nopcommerce/testsuite/LoginTest.java
@@ -0,0 +1,28 @@
+package com.demo.nopcommerce.testsuite;
+
+import com.demo.nopcommerce.loadproperty.LoadProperty;
+import com.demo.nopcommerce.pages.HomePage;
+import com.demo.nopcommerce.pages.LoginPage;
+import com.demo.nopcommerce.testbase.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jay
+ */
+public class LoginTest extends TestBase {
+    LoadProperty loadProperty = new LoadProperty();
+
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void verifyUserShouldNavigateToLoginPage(){
+        homePage.clickOnLoginLink();
+        String expectedText = "Welcome, Please Sign In!";
+        String actualText = loginPage.getWelcomeText();
+        Assert.assertEquals(expectedText, actualText);
+    }
+
+
+}
